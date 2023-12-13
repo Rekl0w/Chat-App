e99753a511456216fd4ccf1921f2e9a1349f8eea
Batuhan Tomo <batuhan.tomo@gmail.com>
2023-12-13 15:08:46 +0300

    Add client and server files

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "XXXXXXXXXXX" } } // change to your private key
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
